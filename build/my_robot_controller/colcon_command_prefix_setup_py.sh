54ebde3f18fbe855c4617a447ce37bfd37f4ba69
Olseda20 <49950899+Olseda20@users.noreply.github.com>
2024-01-11 02:28:29 +0000

    bringing repo up to date 3

--- build/my_robot_controller/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/krono/dev/RobotProgramming/Pothole-Reporting-Robot/install/custom_interfaces/share/custom_interfaces/package.sh"
--- build/my_robot_controller/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/krono/dev/RobotProgramming/Pothole-Reporting-Robot/install/my_robot_controller'
